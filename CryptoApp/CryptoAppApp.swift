3eeeee1025d734e7c278b46b807ccc254b8c06e6
lemauanhduc <tdhuaco372001@gmail.com>
2023-10-17 14:13:00 +0700

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by hanbiro on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
