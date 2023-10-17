14b746e327a272369f1568a3f71aa9b16cf44213
lemauanhduc <tdhuaco372001@gmail.com>
2023-10-17 14:37:29 +0700

    init commit

---
+++ CryptoApp/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by hanbiro on 10/17/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
